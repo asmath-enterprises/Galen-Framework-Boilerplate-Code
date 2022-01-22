8f8095f872156051a7f15486887e3eb107ec10ef
Dinesh <34399732+dnshko@users.noreply.github.com>
2022-01-22 12:28:09 +0530

    setup

--- tests/Config/index.js
+++ tests/Config/index.js
@@ -1,21 +1,21 @@
 // add devices and browsers
 
 var devices = {
-  tablet: {
-    deviceName: "mobile",
-    size: "500x700",
-  },
-  desktop: {
-    deviceName: "desktop",
-    size: "1366x768",
-  },
-};
+	tablet: {
+		deviceName: 'mobile',
+		size: '500x700',
+	},
+	desktop: {
+		deviceName: 'desktop',
+		size: '1366x768',
+	},
+}
 
 var browsers = {
-  firefox: {
-    browserName: "firefox",
-  },
-  chrome: {
-    browserName: "chrome",
-  },
-};
+	firefox: {
+		browserName: 'firefox',
+	},
+	chrome: {
+		browserName: 'chrome',
+	},
+}
--- tests/homepage.test.js
+++ tests/homepage.test.js
@@ -1,28 +1,28 @@
 //import pages
-load("Config/index.js");
-load("Pages/LoginPage.js");
+load('Config/index.js')
+load('Pages/LoginPage.js')
 
 // run test for all devices and browsers
 forAll(devices, function () {
-  forAll(browsers, function () {
-    test("Test on ${browserName} on ${deviceName}", function (device, browser) {
-      // Instantiating the WebDriver
-      var driver = createDriver(
-        "https://tg.billionapps.net/cll-new-2021/",
-        device.size,
-        browser.browserName
-      );
-      //Test code
-      var loginPage = null;
-      logged("Login Page", function () {
-        loginPage = new LoginPage(driver);
-        loginPage.waitForIt();
-        loginPage.loginAs("SwVt9o$mzXDjWOCDsHNBOCLx");
-        // Checking layout on the page
-        checkLayout(driver, "tests/specs/homepage.gspec", device.deviceName);
-      });
-      // Quiting the browser
-      driver.quit();
-    });
-  });
-});
+	forAll(browsers, function () {
+		test('Test on ${browserName} on ${deviceName}', function (device, browser) {
+			// Instantiating the WebDriver
+			var driver = createDriver(
+				'https://tg.billionapps.net/cll-new-2021/',
+				device.size,
+				browser.browserName
+			)
+			//Test code
+			var loginPage = null
+			logged('Login Page', function () {
+				loginPage = new LoginPage(driver)
+				loginPage.waitForIt()
+				loginPage.loginAs('SwVt9o$mzXDjWOCDsHNBOCLx')
+				// Checking layout on the page
+				checkLayout(driver, 'tests/specs/homepage.gspec', device.deviceName)
+			})
+			// Quiting the browser
+			driver.quit()
+		})
+	})
+})
